c9c2bfc4c5ae6002adbbec6ecb8ef18887124d0c
Vitor Camillo <141784471+Camillox2@users.noreply.github.com>
2026-09-04 09:46:19 -0300

    refactor: use integrated YR experience

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import Enhancements from './Enhancements.jsx'
 import './styles.css'
-import './enhancements.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <Enhancements />
   </React.StrictMode>,
 )
