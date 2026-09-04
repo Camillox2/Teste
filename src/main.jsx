f16f7574ebe594b028b57d27c4e599eda72fea02
Vitor Camillo <141784471+Camillox2@users.noreply.github.com>
2026-09-04 08:23:00 -0300

    feat: mount contact form and motion enhancements

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import Enhancements from './Enhancements.jsx'
 import './styles.css'
+import './enhancements.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Enhancements />
   </React.StrictMode>,
 )
