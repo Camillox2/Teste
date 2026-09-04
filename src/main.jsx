2673df5f342ad33614ced95df7a57c0b6c0df0c8
Vitor Camillo <141784471+Camillox2@users.noreply.github.com>
2026-09-04 10:16:33 -0300

    feat: mount Assistente YR

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import AssistantYR from './AssistantYR.jsx'
 import './styles.css'
 import './mobile-fixes.css'
+import './assistant.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <AssistantYR />
   </React.StrictMode>,
 )
